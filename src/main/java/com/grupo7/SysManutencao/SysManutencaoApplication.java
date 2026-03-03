476c2888f27a96b8d474b762255161075307f807
joaozanini <joaozanini.pessoal@gmail.com>
2026-03-02 22:00:42 -0300

    .

---
+++ src/main/java/com/grupo7/SysManutencao/SysManutencaoApplication.java
@@ -0,0 +1,13 @@
+package com.grupo7.SysManutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysManutencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysManutencaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo7/SysManutencao/SysManutencaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo7.SysManutencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysManutencaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
