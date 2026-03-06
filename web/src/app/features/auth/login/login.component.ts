ce19e92486d18e89181ccdc7b1c87012c7f690c8
Lucas Sarnacki Guiraud <lucas.sguiraud@gmail.com>
2026-03-05 21:50:53 -0300

    Mostra senha ao clicar no ícone do olho
    Adicionada a função tooglePassword dentro do component de login para ao clicar no ícone de olho mostre a senha e ao clicar novamente esconde a senha. As imagens trocam conforme aos seus respectivos contextos.

--- web/src/app/features/auth/login/login.component.ts
+++ web/src/app/features/auth/login/login.component.ts
@@ -6,4 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {}
+export class LoginComponent {
+  //
+  togglePassword(){ 
+  //
+  const input_password = document.getElementById('password_input') as HTMLInputElement;
+  const type_password = input_password.type;
+  //
+  const img_password = document.getElementById('icon_olho') as HTMLImageElement;
+  //    
+  if (type_password == "password") {
+    //
+    input_password.type = "text";
+    img_password.src = "/assets/images/icone_olho_aberto.png";
+    //  
+  } else if (type_password == "text") {
+    //
+    input_password.type ="password";
+    img_password.src = "/assets/images/icone_olho_fechado.png";
+
+    //
+  }
+}
+}
